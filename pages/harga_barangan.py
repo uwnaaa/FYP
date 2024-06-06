66809d000f302882df96af90375b2b1b3cf2c359
uwnaaa <101980680+uwnaaa@users.noreply.github.com>
2024-06-06 15:06:45 +0800

    Update harga_barangan.py

--- pages/harga_barangan.py
+++ pages/harga_barangan.py
@@ -6,6 +6,7 @@ df_ayam
 
 ###############################
 import pandas as pd
+df_buah = pd.read_csv('https://raw.githubusercontent.com/uwnaaa/FYP/main/df_buah.csv')
 df_buah = df_buah.drop(columns=['premise', 'address', 'state'])
 
 df_buah
