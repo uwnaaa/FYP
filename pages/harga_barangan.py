a2a816bf1565c5da78f46a7a4abe67722390f74a
uwnaaa <101980680+uwnaaa@users.noreply.github.com>
2024-06-06 13:24:48 +0800

    Update harga_barangan.py

--- pages/harga_barangan.py
+++ pages/harga_barangan.py
@@ -1,6 +1,5 @@
 import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/uwnaaa/FYP/main/df_ayam.csv')
-df_ayam = df_ayam.merge(location, on = 'premise_code', how = 'left')
 df_ayam
 
 
