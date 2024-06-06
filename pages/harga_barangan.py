4db5efd5ce2a2bd4377dedd31660901f2793d97d
uwnaaa <101980680+uwnaaa@users.noreply.github.com>
2024-06-06 13:30:13 +0800

    Update harga_barangan.py

--- pages/harga_barangan.py
+++ pages/harga_barangan.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df_ayam.drop = pd.read_csv('https://raw.githubusercontent.com/uwnaaa/FYP/main/df_ayam.csv')
+df_ayam = pd.read_csv('https://raw.githubusercontent.com/uwnaaa/FYP/main/df_ayam.csv')
 df_ayam.drop(columns=['premise', 'address', 'state'])
 
 
