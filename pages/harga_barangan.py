a3831819338624c8c189e902cc354c45a33eeb2e
uwnaaa <101980680+uwnaaa@users.noreply.github.com>
2024-06-06 15:13:22 +0800

    Update harga_barangan.py

--- pages/harga_barangan.py
+++ pages/harga_barangan.py
@@ -1,12 +1,9 @@
-Price Chicken
-
 import pandas as pd
 df_ayam = pd.read_csv('https://raw.githubusercontent.com/uwnaaa/FYP/main/df_ayam.csv')
 df_ayam = df_ayam.drop(columns=['premise', 'address', 'state'])
 
 df_ayam
 
-Price Fruit
 ###############################
 import pandas as pd
 df_buah = pd.read_csv('https://raw.githubusercontent.com/uwnaaa/FYP/main/df_buah.csv')
@@ -14,7 +11,6 @@ df_buah = df_buah.drop(columns=['premise', 'address', 'state'])
 
 df_buah
 
-Price vegetables
 ###############################
 import pandas as pd
 df_sayur = pd.read_csv('https://raw.githubusercontent.com/uwnaaa/FYP/main/df_sayur.csv')
