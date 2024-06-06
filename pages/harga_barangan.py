fef5b7e9bd518d8df7dbd8eddfb0b5231f51a7fc
uwnaaa <101980680+uwnaaa@users.noreply.github.com>
2024-06-06 13:04:52 +0800

    Update harga_barangan.py

--- pages/harga_barangan.py
+++ pages/harga_barangan.py
@@ -1,2 +1,11 @@
+# If not already installed, do: pip install pandas fastparquet
+import pandas as pd
+
+URL_DATA = 'https://storage.data.gov.my/pricecatcher/lookup_premise.parquet'
+
+location = pd.read_parquet(URL_DATA)
+if 'date' in location.columns: location['date'] = pd.to_datetime(location['date'])
+
+print(location)
 df_ayam = df_ayam.merge(location, on = 'premise_code', how = 'left')
 df_ayam
