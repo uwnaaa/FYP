280708e6429e257cd8245f6b46e019c96a9bba40
uwnaaa <101980680+uwnaaa@users.noreply.github.com>
2024-06-23 16:29:29 +0800

    Update price_selangor_district.py

--- pages/price_selangor_district.py
+++ pages/price_selangor_district.py
@@ -202,9 +202,36 @@ location = pd.read_parquet(URL_DATA)
 if 'date' in location.columns: location['date'] = pd.to_datetime(location['date'])
 
 print(location)
-df_ayam = df_ayam.merge(location, on = 'premise_code', how = 'left')
 df_ayam.drop(columns=['premise', 'address', 'state'])
 df_ayam
+
+
+##########################################
+st.header('DF Buah', divider='rainbow')
+import pandas as pd
+
+URL_DATA = 'https://storage.data.gov.my/pricecatcher/lookup_premise.parquet'
+
+location = pd.read_parquet(URL_DATA)
+if 'date' in location.columns: location['date'] = pd.to_datetime(location['date'])
+
+print(location)
+df_buah.drop(columns=['premise', 'address', 'state'])
+df_buah
+
+
+############################################
+st.header('DF Sayur', divider='rainbow')
+import pandas as pd
+
+URL_DATA = 'https://storage.data.gov.my/pricecatcher/lookup_premise.parquet'
+
+location = pd.read_parquet(URL_DATA)
+if 'date' in location.columns: location['date'] = pd.to_datetime(location['date'])
+
+print(location)
+df_sayur.drop(columns=['premise', 'address', 'state'])
+df_sayur
 
 
 
