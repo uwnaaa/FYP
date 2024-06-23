2182a8ebdfafef70ed6cfc8e8671e5ee703fa57c
uwnaaa <101980680+uwnaaa@users.noreply.github.com>
2024-06-23 16:23:42 +0800

    Update price_selangor_district.py

--- pages/price_selangor_district.py
+++ pages/price_selangor_district.py
@@ -192,6 +192,20 @@ correlation_matrix = df_selected.corr()
 (correlation_matrix)
 
 
+############################################
+st.header('DF Ayam', divider='rainbow')
+import pandas as pd
+
+URL_DATA = 'https://storage.data.gov.my/pricecatcher/lookup_premise.parquet'
+
+location = pd.read_parquet(URL_DATA)
+if 'date' in location.columns: location['date'] = pd.to_datetime(location['date'])
+
+print(location)
+df_ayam = df_ayam.merge(location, on = 'premise_code', how = 'left')
+df_ayam
+
+
 
 
 
