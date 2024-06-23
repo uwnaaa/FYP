c85f8a66eb24f1e6b6c5176a4e6b14e3a2bc9341
uwnaaa <101980680+uwnaaa@users.noreply.github.com>
2024-06-23 16:25:29 +0800

    Update price_selangor_district.py

--- pages/price_selangor_district.py
+++ pages/price_selangor_district.py
@@ -202,9 +202,7 @@ location = pd.read_parquet(URL_DATA)
 if 'date' in location.columns: location['date'] = pd.to_datetime(location['date'])
 
 print(location)
-df_ayam = df_ayam.merge(location, on = 'premise_code', how = 'left')
-df_ayam
-
+df_ayam.drop(columns=['premise', 'address', 'state'])
 
 
 
