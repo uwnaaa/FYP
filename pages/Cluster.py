422c8ae4cb42f58ca53c6fec050e6b47866c062f
uwnaaa <101980680+uwnaaa@users.noreply.github.com>
2024-06-30 10:24:55 +0800

    Update Cluster.py

--- pages/Cluster.py
+++ pages/Cluster.py
@@ -6,6 +6,7 @@ df = pd.read_parquet(url)
 
 ####################################################################
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import StandardScaler
 
 # Select relevant columns for clustering
