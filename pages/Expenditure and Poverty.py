538da417c7ed85621ed5efa0038f835f219cfa57
uwnaaa <101980680+uwnaaa@users.noreply.github.com>
2024-06-30 10:09:45 +0800

    Update Expenditure and Poverty.py

--- pages/Expenditure and Poverty.py
+++ pages/Expenditure and Poverty.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 col1,col2 = st.columns([1,2])
-col1.title('Sum:')
+col1.title('Poverty Analysis:')
 
 with st.form('addition'):
-    a = st.number_input('expenditure')
-    b = st.number_input('poverty')
-    submit = st.form_submit_button('add')
+    a = st.number_input('Income')
+    b = st.number_input('Expenditure')
+    submit = st.form_submit_button('Submit')
 
 if submit:
     col2.title(f'{a+b:.2f}')
