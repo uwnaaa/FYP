767c4587c4bbb69310f5e5b6c7d6c91159e2bc76
uwnaaa <101980680+uwnaaa@users.noreply.github.com>
2024-06-30 12:56:51 +0800

    Update Expenditure and Poverty.py

--- pages/Expenditure and Poverty.py
+++ pages/Expenditure and Poverty.py
@@ -7,12 +7,16 @@ col1,col2 = st.columns([1,2])
 col1.title('Poverty Rate Analysis')
 
 with st.form('addition'):
-    a = st.number_input('Income')
-    b = st.number_input('Expenditure')
+    Income = st.number_input('Income')
+    Expenditure = st.number_input('Expenditure')
     submit = st.form_submit_button('Submit')
 
 if submit:
-    col2.title(f'{a+b:.2f}')
+    poverty_rate, cluster = predict_poverty_rate_and_cluster(income, expenditure)
+
+        # Display results
+        st.write(f'Predicted Poverty Rate: {poverty_rate}%')
+        st.write(f'Cluster: {cluster}')
 
 # Load your model from the pkl file
 model = joblib.load('pages/clustering_model.pkl')
@@ -23,14 +27,7 @@ def predict_poverty_rate_and_cluster(income, expenditure):
     poverty_rate, cluster = model.predict([[income, expenditure]])
     return poverty_rate[0], cluster[0]
 
- # Submit button
-    if st.button('Predict'):
-        poverty_rate, cluster = predict_poverty_rate_and_cluster(income, expenditure)
 
-        # Display results
-        st.write(f'Predicted Poverty Rate: {poverty_rate}%')
-        st.write(f'Cluster: {cluster}')
 
-if __name__ == '__main__':
-    main()
+
 
