12e10a13ddd2d26385c5841852cb57d82988cac3
uwnaaa <101980680+uwnaaa@users.noreply.github.com>
2024-06-30 13:10:56 +0800

    Update Expenditure and Poverty.py

--- pages/Expenditure and Poverty.py
+++ pages/Expenditure and Poverty.py
@@ -2,7 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+# Load your model from the pkl file
+model = joblib.load('pages/clustering_model.pkl')
 
+# Function to predict poverty rate and cluster
+def predict_poverty_rate_and_cluster(income, expenditure):
+    # Assuming model.predict() returns a tuple (poverty_rate, cluster)
+    poverty_rate, cluster = model.predict([[income, expenditure]])
+    return poverty_rate[0], cluster[0]
 col1,col2 = st.columns([1,2])
 col1.title('Poverty Rate Analysis')
 
@@ -14,18 +21,11 @@ with st.form('addition'):
 if submit:
     poverty_rate, cluster = predict_poverty_rate_and_cluster(income, expenditure)
 
-    # Display results
-    st.write(f"Predicted Poverty Rate:, {poverty_rate}%")
-    st.write(f"Cluster:, {cluster}")
+# Display results
+st.write(f"Predicted Poverty Rate:, {poverty_rate}%")
+st.write(f"Cluster:, {cluster}")
 
-# Load your model from the pkl file
-model = joblib.load('pages/clustering_model.pkl')
 
-# Function to predict poverty rate and cluster
-def predict_poverty_rate_and_cluster(income, expenditure):
-    # Assuming model.predict() returns a tuple (poverty_rate, cluster)
-    poverty_rate, cluster = model.predict([[income, expenditure]])
-    return poverty_rate[0], cluster[0]
 
 
 
